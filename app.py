aadfff82952804e481758aa5e044df87b3195782
Monish Agarwal <monu1510agarwal@gmail.com>
2026-04-17 12:46:02 +0530

    before deplying

--- app.py
+++ app.py
@@ -291,4 +291,9 @@ def export_note():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
